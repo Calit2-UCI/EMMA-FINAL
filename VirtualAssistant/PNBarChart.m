9216397cfdf6bee7a795e476d76042b63f3183cf
Gilbert Wang <gilbertwang0159@gmail.com>
2016-11-09 02:32:56 -0800

    Update PNBarChart.m

--- VirtualAssistant/PNBarChart.m
+++ VirtualAssistant/PNBarChart.m
@@ -50,7 +50,7 @@
     _showLabel           = YES;
     _barBackgroundColor  = PNLightGrey;
     _labelTextColor      = [UIColor grayColor];
-    _labelFont           = [UIFont systemFontOfSize:11.0f];
+    _labelFont           = [UIFont systemFontOfSize:10.0f];
     _xChartLabels        = [NSMutableArray array];
     _yChartLabels        = [NSMutableArray array];
     _bars                = [NSMutableArray array];
